38fce6c914c16b3b079bd24bf38cd575e5b74d26
Cristobal Lecaros <clecarosc@gmail.com>
2019-11-21 15:35:53 -0300

    tablas de datos por region y por servicio

--- R/analysis/tables_processed_data.R
+++ R/analysis/tables_processed_data.R
@@ -8,6 +8,31 @@ casos_nacional <- bind_rows(list(nac1, nac2)) %>% arrange(year, edad, sexo)
 write_csv(casos_nacional, "casos_nacional.csv")
 saveRDS(casos_nacional, "casos_nacional.rds")
 
+casos_nacional
+
 #Regional
+
+regional1 <- df_casos_py %>% group_by(F_ENTRADA, region, edad, sexo) %>% summarise(cases = sum(cases), 
+                                                                      py = sum(py)) %>% rename(year = F_ENTRADA) %>% ungroup()
+
+regional2 <- regional1 %>% group_by(year, region, edad) %>% summarise_if(is.numeric, sum) %>% add_column(sexo = "T", .after = "edad") %>% ungroup()
+
+casos_regional <- bind_rows(list(regional1, regional2)) %>% arrange(year, region, edad, sexo)
+write_csv(casos_regional, "casos_regional.csv")
+saveRDS(casos_regional, "casos_regional.rds")
+
+#Servicios
+df_casos_py
+serv1 <- df_casos_py %>% group_by(F_ENTRADA, servicio, sexo, edad) %>% summarise(cases = sum(cases), 
+                                                                                   py = sum(py)) %>% rename(year = F_ENTRADA) %>% ungroup()
+serv2 <- serv1 %>% group_by(year, servicio, edad) %>% summarise_if(is.numeric, sum) %>% add_column(sexo = "T", .after = "edad") %>% ungroup()
+
+casos_servicio <- bind_rows(list(serv1, serv2)) %>% arrange(year, servicio, edad, sexo)
+write_csv(casos_servicio, "casos_servicio.csv")
+saveRDS(casos_servicio, "casos_servicio.rds")
+
+
+
+
 
 
