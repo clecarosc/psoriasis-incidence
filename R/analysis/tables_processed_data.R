02fc493dfb632280e5133c00bd6cf487dcad88f4
Cristobal Lecaros <clecarosc@gmail.com>
2019-11-21 14:38:11 -0300

    datos psor procesados nacional 16-17

--- R/analysis/tables_processed_data.R
+++ R/analysis/tables_processed_data.R
@@ -6,6 +6,7 @@ nac2 <- nac1 %>% group_by(year, edad) %>% summarise_if(is.numeric, sum) %>% add_
 
 casos_nacional <- bind_rows(list(nac1, nac2)) %>% arrange(year, edad, sexo)
 write_csv(casos_nacional, "casos_nacional.csv")
+saveRDS(casos_nacional, "casos_nacional.rds")
 
 #Regional
 
