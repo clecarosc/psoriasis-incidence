d4bec06270712292d6b133e31317e3847b1a1319
Cristobal Lecaros <clecarosc@gmail.com>
2019-11-24 13:45:10 -0300

    analisis validacion segun documentacion tesis

--- R/validation/validacion.R
+++ R/validation/validacion.R
@@ -2,6 +2,7 @@ library(tidyverse)
 library(stringr)
 
 validation <- read_delim("psoriasis_primera_consulta.xls", ";")
+glimpse(validation)
 validation <- validation %>% select(`Causal de Egreso`,`Tipo Profesional solicitante`,
                                     `Centro Origen`, `Sospecha Diagóstica`, `Primera Atención`,
                                     `Fecha Primera Atención`, `Fecha Alta`,
@@ -13,54 +14,41 @@ validation$`Fecha Primera Atención` <- str_replace(validation$`Fecha Primera At
 sum(!is.na(validation$`Fecha Primera Atención`)) # 377 datos tienen fecha
 sum(is.na(validation$`Fecha Primera Atención`)) # 681/1058 no tienen
 unique(validation$`Fecha Primera Atención`)
-#datos durante el periodo de observacion del estudio 13-17
-validation_13_17 <- validation %>% filter(`Fecha Primera Atención` %in% c(2013,2014,2015,2016,2017))
-
-sum(is.na(validation_13_17$`Fecha Primera Atención`)) # 0
-sum(!is.na(validation_13_17$`Fecha Primera Atención`)) # 217 datos tienen fecha durante el periodo
-
-table(validation_13_17$`Fecha Primera Atención`)
 
 table(validation$`Fecha Primera Atención`)
-table(validation$`Fecha Primera Atención`)
+
 
 ### causas de egreso: 7 posibles respuestas
-ggplot(validation_13_17, aes(x= factor(`Causal de Egreso`))) +
+validation$`Causal de Egreso` <- factor(validation$`Causal de Egreso`)
+
+egreso <- validation %>% group_by(`Causal de Egreso`) %>% summarise( N = n()) %>% arrange(N)
+sum(egreso$N)
+
+ggplot(egreso, aes(x= `Causal de Egreso`, y = n)) +
   geom_bar() +
   coord_flip()
 
 ### tipo de profesional solicitante: medico, otro, matrona
-ggplot(validation_13_17, aes(x= factor(`Tipo Profesional solicitante`))) +
+ggplot(validation, aes(x= factor(`Tipo Profesional solicitante`))) +
   geom_bar() +
   coord_flip()
 
-#se filtra solo las ic hechas por medicos (80 ic, todas el 2017)
-medicos <- validation_13_17 %>% filter(`Tipo Profesional solicitante` == "Médico")
+#se filtra solo las ic hechas en el 2017 (184) o hechas por medicos (80 ic, todas el 2017)
+todas <- validation %>% filter(`Fecha Primera Atención` == 2017) %>% select(`Sospecha Diagóstica`, `Primera Atención`, `Atención Alta`, `Causal de Egreso`)
+medicos <- validation %>%
+  filter(`Tipo Profesional solicitante` == "Médico" & `Fecha Primera Atención` == 2017) %>% 
+  select(`Sospecha Diagóstica`, `Primera Atención`, `Atención Alta`)
 
-#todas el 2017
-table(medicos$`Fecha Primera Atención`)
+# la mayoria tienen causa antecion realizada, algunas sin comentario
+table(todas$`Causal de Egreso`)
 
-#se revisan manualmente las sospechas diagnosticas y el comentario de primera atencion y de atencion al alta
-medicos$`Sospecha Diagóstica` # 80 casos, cuantos dicen control 45,80: 2/80; los que se piensa otro dg:10,118,28,29,34,37,38,41,46,52,54,73.
-medicos_1aten <- medicos %>% drop_na(`Primera Atención`); medicos_1aten$`Primera Atención`
+#se revisan manualmente 3 cols: sospecha diagnostica, primera atencion y atencion alta
+todas
+medicos
+
+#se registra cuales casos no corresponde
+##los que se piensa otro dg:10,118,28,29,34,37,38,41,46,52,54,73.
 ##de los 75 comentarios de primera atencion, 1(22 es tto acne-rosacea), 1 estudio (26), 1 dice hola(31), 1 dice ic derm(32), 1 celulitis,
 ## 1 es artr psor humira ley ricarte soto[35], 1 es psor+celulitis [36],1 es dermatitis de contacto manos [37], 1 es un bcc [38],
 ## 1 es celulitis+psor [39], 1 es psor-dermatitis de panañal [44], bowen vs psor [46], dermatofitosis [49], keratosis actinica [50],
 ## 1 control  [51], dishidrosis palmar [52], estudio [62], no registra dato x 3 [64-66], "s" [70], liquen plano pilaris [72]
-
-medicos_alta <- medicos %>% drop_na(`Atención Alta`); medicos_alta$`Atención Alta`
-
-# la mayoria tienen causa antecion realizada, algunas sin comentario
-ggplot(medicos, aes(x= factor(`Causal de Egreso`))) +
-  geom_bar() +
-  coord_flip()
-
-
-
-#hay un caso de no pertinencia solicitado por otro profesional no medico
-no_pert <- validation_13_17 %>% filter(`Causal de Egreso` == "No pertinencia")
-no_pert$`Sospecha Diagóstica`;no_pert$`Primera Atención`
-
-###############
-### otro enfoque, sin considerar la fecha de atencion, solo drop los que no tienen comentario ()
-validation_comentario <- validation  %>%  drop_na(`Atención Alta`)
